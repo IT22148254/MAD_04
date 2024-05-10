bd435476c8450ff883928b699c7675e613d10164
Praboth Sandaruwan <bleezbub12@gmail.com>
2024-05-10 08:11:06 +0530

    Drawer layout

--- app/src/main/java/com/example/mad_lab_04/MainActivity.kt
+++ app/src/main/java/com/example/mad_lab_04/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.example.mad_lab_04
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.mad_lab_04.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        actionBarDrawerToggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open_nav, R.string.close_nav)
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val menuIcon = findViewById<ImageView>(R.id.img_nav)
+        menuIcon.setOnClickListener {
+            if (drawerLayout.isDrawerOpen(findViewById(R.id.nav_view))) {
+                drawerLayout.closeDrawer(findViewById(R.id.nav_view))
+            } else {
+                drawerLayout.openDrawer(findViewById(R.id.nav_view))
+            }
+        }
+    }
+
+    override fun onOptionsItemSelected(item: android.view.MenuItem): Boolean {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
